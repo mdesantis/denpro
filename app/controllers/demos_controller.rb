6dc258f02504a37923309ea285af26909f266cba
Maurizio De Santis <desantis.maurizio@gmail.com>
2025-03-23 16:10:59 +0100

    wip

--- app/controllers/demos_controller.rb
+++ app/controllers/demos_controller.rb
@@ -3,6 +3,10 @@ class DemosController < ApplicationController
   end
 
   def sendfile_demo
-    send_file Rails.root.join('vendor', '.keep')
+    storage_file_path = Rails.root.join('storage').find do
+      break it if FileTest.file?(it) && it.basename.to_s != '.keep'
+    end
+
+    send_file storage_file_path
   end
 end
