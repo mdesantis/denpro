af7962493ead8204480ae271f371a8f2a2ee2daa
Maurizio De Santis <desantis.maurizio@gmail.com>
2025-07-23 11:24:10 +0200

    Add emotion cache to MUI dashboard template

--- app/frontend/lib/mui_templates/v7.2.0/dashboard/Dashboard.tsx
+++ app/frontend/lib/mui_templates/v7.2.0/dashboard/Dashboard.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import { CacheProvider } from '@emotion/react';
+import { EmotionCache } from '@emotion/cache';
+import createEmotionCache from '~/lib/create_emotion_cache';
 import type {} from '@mui/x-date-pickers/themeAugmentation';
 import type {} from '@mui/x-charts/themeAugmentation';
 import type {} from '@mui/x-data-grid-pro/themeAugmentation';
@@ -26,38 +29,42 @@ const xThemeComponents = {
   ...treeViewCustomizations,
 };
 
-export default function Dashboard(props: { disableCustomTheme?: boolean }) {
+export default function Dashboard(props: { disableCustomTheme?: boolean, emotionCache?: EmotionCache }) {
+  const cache = props.emotionCache ?? createEmotionCache()
+
   return (
-    <AppTheme {...props} themeComponents={xThemeComponents}>
-      <CssBaseline enableColorScheme />
-      <Box sx={{ display: 'flex' }}>
-        <SideMenu />
-        <AppNavbar />
-        {/* Main content */}
-        <Box
-          component="main"
-          sx={(theme) => ({
-            flexGrow: 1,
-            backgroundColor: theme.vars
-              ? `rgba(${theme.vars.palette.background.defaultChannel} / 1)`
-              : alpha(theme.palette.background.default, 1),
-            overflow: 'auto',
-          })}
-        >
-          <Stack
-            spacing={2}
-            sx={{
-              alignItems: 'center',
-              mx: 3,
-              pb: 5,
-              mt: { xs: 8, md: 0 },
-            }}
+    <CacheProvider value={cache}>
+      <AppTheme {...props} themeComponents={xThemeComponents}>
+        <CssBaseline enableColorScheme />
+        <Box sx={{ display: 'flex' }}>
+          <SideMenu />
+          <AppNavbar />
+          {/* Main content */}
+          <Box
+            component="main"
+            sx={(theme) => ({
+              flexGrow: 1,
+              backgroundColor: theme.vars
+                ? `rgba(${theme.vars.palette.background.defaultChannel} / 1)`
+                : alpha(theme.palette.background.default, 1),
+              overflow: 'auto',
+            })}
           >
-            <Header />
-            <MainGrid />
-          </Stack>
+            <Stack
+              spacing={2}
+              sx={{
+                alignItems: 'center',
+                mx: 3,
+                pb: 5,
+                mt: { xs: 8, md: 0 },
+              }}
+            >
+              <Header />
+              <MainGrid />
+            </Stack>
+          </Box>
         </Box>
-      </Box>
-    </AppTheme>
+      </AppTheme>
+    </CacheProvider>
   );
 }
