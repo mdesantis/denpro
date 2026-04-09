6348a098ccbfecdb9d898ac6ef38204a5b38ff61
Maurizio De Santis <desantis.maurizio@gmail.com>
2026-04-09 12:53:43 +0200

    Make glob patterns portable

--- app/frontend/lib/ssr.jsx
+++ app/frontend/lib/ssr.jsx
@@ -2,7 +2,7 @@ import  { StrictMode } from 'react'
 import { renderToString } from 'react-dom/server'
 import createEmotionCache from '~/lib/create_emotion_cache'
 
-const components = import.meta.glob('~/components/**/*.(j|t)sx', { eager: true })
+const components = import.meta.glob('~/components/**/*.{j,t}sx', { eager: true })
 
 export function render(name, props) {
   const componentModule = components[`/components/${name}.jsx`] ?? components[`/components/${name}.tsx`]
