66d9fd440cf714ec713445d90c87825a4290362c
Maurizio De Santis <desantis.maurizio@gmail.com>
2026-05-10 18:13:22 +0200

    refactor: remove dev-only SSR Deno config from initializer

--- config/initializers/ssr_deno.rb
+++ config/initializers/ssr_deno.rb
@@ -6,12 +6,6 @@ Rails.application.config.ssr_deno.bundles = {
 
 Rails.application.config.ssr_deno.node_builtins_enabled = true
 
-Rails.application.config.ssr_deno.auto_reload = Rails.env.development?
-
-Rails.application.config.ssr_deno.raise_on_bundle_error = !Rails.env.production?
-
-Rails.application.config.ssr_deno.raise_on_render_error = !Rails.env.production?
-
 ActiveSupport::Notifications.subscribe('ssr_render.ssr_deno') do |_name, started, finished, _id, payload|
   duration = ((finished - started) * 1000).round(1)
   Rails.logger.info "  Completed SSR request (Duration: #{duration}ms | Bundle: #{payload[:bundle_name].inspect})"
